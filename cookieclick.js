8d4a5f4d153caf3b0d083a98836cc69db63239a7
cyril-bonneau <cyril.bonneau@ynov.com>
2018-05-02 14:53:49 +0200

    du js

--- cookieclick.js
+++ cookieclick.js
@@ -1,12 +1,68 @@
-var cookiedisponible
-function clicker(){
-    if(typeof(Storage)!=="undefined"){
-        if(localStorage.clicker){
-            localStorage.clicker = number(localStorage.clicker) +1;
+var cookiedisponible;
+var totalcookie;
+
+function statsauver() {
+    localStorage.clicker = totalcookie
+    document.getElementById("total").innerHTML = totalcookie + " cookie farmé"
+}
+
+function clicker() {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clicker) {
+            localStorage.clicker = Number(localStorage.clicker) + 1;
         }
-        else{
+        else {
             localStorage.clicker = 0;
         }
-        document.getElementById("result").innerHTML = + localStorage.clicker + " cookie disponible"
+        document.getElementById("cookie").innerHTML = localStorage.clicker + " cookie disponible"
+    }
+}
+
+function Marmotte() {
+    console.log('ici')
+    try {
+        if (localStorage.getItem('clicker') > 10) {
+            let count = localStorage.getItem('clicker');
+            setInterval(_ => {
+                localStorage.setItem('clicker', count++);
+            }, 1000)
+        } else {
+            alert("pas assez de cookie du shnaps")
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function JeanKevin() {
+    console.log('ici')
+    try {
+        if (localStorage.getItem('clicker') > 10) {
+            localStorage.setItem('clicker', count2-10)
+            let count2 = localStorage.getItem('clicker');
+            setInterval(_ => {
+                localStorage.setItem('clicker', count2+2);
+            }, 1000)
+        } else {
+            alert("pas assez de cookie du shnaps")
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+function JeanKevin() {
+    console.log('ici')
+    try {
+        if (localStorage.getItem('clicker') > 10) {
+            let count2 = localStorage.getItem('clicker');
+            setInterval(_ => {
+                localStorage.setItem('clicker', count2+2);
+            }, 1000)
+        } else {
+            alert("pas assez de cookie du shnaps")
+        }
+    } catch (err) {
+        console.error(err)
     }
 }
